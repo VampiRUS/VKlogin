46bd987c2b12ecb1efe746ba1cbd04168f7a0270
Kravtsov Nikolai <vampirus.666@gmail.com>
2011-06-17 10:16:33 +0900

    hide php notification error

--- com_vklogin/site/controller.php
+++ com_vklogin/site/controller.php
@@ -55,9 +55,9 @@ class VkloginController extends JController
 			$vkConfig = &JComponentHelper::getParams( 'com_vklogin' );
 			//joomla 1.6. form
 			$requestData = JRequest::getVar('jform', array(), 'post', 'array');
-			$username = JRequest::getString('username', $requestData['username'], 'post');
-			$name = JRequest::getString('name', $requestData['name'], 'post');
-			$email = JRequest::getString('email', $requestData['email'], 'post');
+			$username = JRequest::getString('username', @$requestData['username'], 'post');
+			$name = JRequest::getString('name', @$requestData['name'], 'post');
+			$email = JRequest::getString('email', @$requestData['email'], 'post');
 			if ($vkConfig->get('silentreg') && !$email && !$vkConfig->get( 'jomsocial' )){
 				$email = JRequest::getString('domain', '', 'post').'@vk.com';
 			} 
